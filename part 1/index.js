d2c57674413f1e4961f48e89953ec5d7d06ab3b2
pradeljc@gmail.com <pradeljc@gmail.com>
2018-12-18 22:23:54 +0800

    finished part 1 and 2 of the exam

---
+++ part 1/index.js
@@ -0,0 +1,105 @@
+/*
+  Part 1: Javascript Coding Challenge
+ */
+
+
+ /*
+ 1. Create a function that can breakdown the bills in a given amount
+	Example:
+		input: 2653
+		output: {
+			"1000": 2,
+			"500": 1,
+			"100": 1,
+			"50" : 1
+		}
+ */
+
+
+
+
+ // Enter your code here
+
+
+
+
+
+ /*
+2. Given the object:
+
+let categories = [
+	{ id:'animals', parent: null },
+	{ id:'mammals', parent: 'animals' },
+	{ id:'cats', parent: 'mammals' },
+	{ id:'dogs', parent: 'mammals' },
+	{ id:'chihuahua', parent: 'dogs' },
+	{ id:'labrador', parent: 'dogs' },
+	{ id:'persian', parent: 'cats' },
+	{ id:'siamese', parent: 'cats' }
+]
+
+Create a function to mutate it to output:
+
+{
+  	"animals": {
+		"mammals": {
+			"cats": {
+				"persian": {},
+				"siamese": {}
+			},
+			"dogs": {
+				"chihuahua": {},
+				"labrador": {}
+			}
+		}
+  	}
+}
+ */
+
+
+/*
+ 3. Using the JSONPlaceholder service(http://jsonplaceholder.typicode.com/), create a function that makes 
+	 http requests from http://jsonplaceholder.typicode.com/users and http://jsonplaceholder.typicode.com/posts 
+	 and then create the following json object format from the results:
+		"userPosts": [
+			{
+				"name": "Leanne Graham",
+				"username": "Bret",
+				"email": "Sincere@april.biz",
+				"posts": [
+					{
+						"title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+						"body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+					},
+					{
+						"title": "qui est esse",
+						"body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+					},
+					...and so on
+				]
+			},
+			{
+				"name": "Ervin Howell",
+				"username": "Antonette",
+				"email": "Shanna@melissa.tv",
+				"posts": [
+					{
+						"title": "in quibusdam tempore odit est dolorem",
+						"body": "itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio"
+					},
+					{
+						"title": "dolorum ut in voluptas mollitia et saepe quo animi",
+						"body": "aut dicta possimus sint mollitia voluptas commodi quo doloremque\niste corrupti reiciendis voluptatem eius rerum\nsit cumque quod eligendi laborum minima\nperferendis recusandae assumenda consectetur porro architecto ipsum ipsam"
+					},
+					...and so on
+				]
+			},
+			...and so on
+		]
+ */
+
+
+ // Enter your code here
+
+
+
